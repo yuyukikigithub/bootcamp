7814f97574a6c303c9e7d5ee7522cea53b80942d
yuki.lau <szeki6578@gmail.com>
2023-12-01 23:21:46 +0800

    20231201 night update

--- java-exercise/src/JavaQuest12.java
+++ java-exercise/src/JavaQuest12.java
@@ -23,22 +23,19 @@ public class JavaQuest12 {
     int[] answers = twoSum(new int[] { 2, 7, 11, 15 }, 9); // {2, 7, 11, 15} is the dataset, 9 is the target to sum
     int[] answers2 = twoSum(new int[] { 3, 2, 4 }, 6); // {3, 2, 4} is the dataset, 6 is the target to sum
     int[] answers3 = twoSum(new int[] { 3, 3 }, 7); // {3, 3} is the dataset, 7 is the target to sum
-    System.out.println(Arrays.toString(answers2));
+    System.out.println(Arrays.toString(answers));
   }
 
   // Code a method here to return an array of two numbers that sum up to the
   // target
   public static int[] twoSum(int[] arr, int target) {
-    int[] newArr = new int[2]; 
+    int[] newArr = new int[] {-1,-1}; 
     for (int i = 0; i < arr.length; i++) {
       for (int j = 0; j < arr.length; j++) {
-        if (arr[i]+arr[j]==target) {
+        if (arr[i]+arr[j]==target && arr[i]!=arr[j]) {
           newArr[0] = i;
           newArr[1] = j;
           break;
-        }else {
-          newArr[0] = -1;
-          newArr[1] = -1;
         }
       }
     }
